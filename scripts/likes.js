ab72f5d3bc447135add1af8aaea2dc28668691bd
Bjoern <onoofelogmail.com>
2026-03-10 17:35:31 +0100

    added 2nd version of like-icon function

--- scripts/likes.js
+++ scripts/likes.js
@@ -1,16 +1,13 @@
-let likedByU = false;
+let likedByU;
 
 function toggleLikes(index){
+    likedByU = books[index].liked;
+    let bookID = "book-likes-" + index;
+    let bookLikes = document.getElementById(bookID).innerHTML;
     if (likedByU == false) {
-        likedByU = true;
-        let bookID = "book-likes-" + index;
-        let bookLikes = document.getElementById(bookID).innerHTML;
         bookLikes++;
         document.getElementById(bookID).innerHTML = bookLikes;
     } else{
-        likedByU = false;
-        let bookID = "book-likes-" + index;
-        let bookLikes = document.getElementById(bookID).innerHTML;
         bookLikes--;
         document.getElementById(bookID).innerHTML = bookLikes;
     }
@@ -19,9 +16,11 @@ function toggleLikes(index){
 
 function switchLikedIcon(index){
     let iconID = "book-likes-icon-" + index;
-    if (likedByU == true) {
+    if (likedByU == false) {
         document.getElementById(iconID).src="./assets/img/icon-like-on.png";
+        books[index].liked = true;
     } else{
         document.getElementById(iconID).src="./assets/img/icon-like-off.png";
+        books[index].liked = false;
     }
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,10 +1,15 @@
 function bookDetails(name, price, likes, author, releaseDate, genre, i){
 document.getElementById('main-container').innerHTML += `
             <section class="book-base" id="book-${i}">
+            
                 <h2 id="book-title-">${name}</h2>
+
                 <div class="separator"></div>
+
                 <img class="icon-book" src="./assets/img/icon-book.png" alt="">
+
                 <div class="separator"></div>
+
                 <section class="price-n-likes">
                     <p id="book-price-${i}">${price}€</p>
                     <div class="book-likes">
@@ -12,6 +17,7 @@ document.getElementById('main-container').innerHTML += `
                         <a href="#" onclick="toggleLikes(${i})"><img id="book-likes-icon-${i}" class="icon-like" src="./assets/img/icon-like-off.png" alt=""></a>
                     </div>
                 </section>
+
                 <section class="book-info">
                     <table class="book-info-table">
                         <tr>
@@ -28,15 +34,19 @@ document.getElementById('main-container').innerHTML += `
                         </tr>
                     </table>
                 </section>
+
                 <div class="separator"></div>
+
                 <section class="book-comments" id="book-comments-${i}">
                     <h3>Comments</h3>
                     <p id="my-comment">It is a book of all time 100%.</p>
                 </section>
+
                 <section class="book-footer">
                     <input class="book-comment-input" id="comment-input-${i}" type="text" value="enter your comment...">
                     <a href="#" onclick="addComment(${i})"><img class="icon-submit" src="./assets/img/ivon-submit.png" alt=""></a>
                 </section>
+
             </section>
            `;
 }
