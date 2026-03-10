54740e0295d9f0b467e84042ba687d8dda3dff21
Bjoern <onoofelogmail.com>
2026-03-10 16:37:07 +0100

    added like function

--- scripts/likes.js
+++ scripts/likes.js
@@ -1,6 +1,17 @@
+let likedByU = false;
+
 function toggleLikes(index){
-    let bookID = "book-likes-" + index;
-    let bookLikes = document.getElementById(bookID).innerHTML;
-    bookLikes++;
-    document.getElementById(bookID).innerHTML = bookLikes;
+    if (likedByU == false) {
+        likedByU = true;
+        let bookID = "book-likes-" + index;
+        let bookLikes = document.getElementById(bookID).innerHTML;
+        bookLikes++;
+        document.getElementById(bookID).innerHTML = bookLikes;
+    } else{
+        likedByU = false;
+        let bookID = "book-likes-" + index;
+        let bookLikes = document.getElementById(bookID).innerHTML;
+        bookLikes--;
+        document.getElementById(bookID).innerHTML = bookLikes;
+    }
 }
