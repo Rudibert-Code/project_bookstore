55777e43b7fc147975b11d614b34779e8ab76df5
Bjoern <onoofelogmail.com>
2026-03-11 13:15:58 +0100

    added function to set like-icons after 1st render.

--- scripts/likes.js
+++ scripts/likes.js
@@ -1,5 +1,17 @@
 let likedByU;
 
+// check object if book is liked and turn Like-Icon on/off after 1st render. 
+function firstLickedIcon(index){
+    likedByU = books[index].liked;
+    let iconID = "book-likes-icon-" + index;
+    if (likedByU == false) {
+        document.getElementById(iconID).src="./assets/img/icon-like-off.png";
+    } else{
+        document.getElementById(iconID).src="./assets/img/icon-like-on.png";
+    }
+}
+
+// on click check object if book is liked, then increase or decrease likes-number... 
 function toggleLikes(index){
     likedByU = books[index].liked;
     let bookID = "book-likes-" + index;
@@ -14,6 +26,7 @@ function toggleLikes(index){
     switchLikedIcon(index);
 }
 
+// ...and turn Like-Icon on/off.
 function switchLikedIcon(index){
     let iconID = "book-likes-icon-" + index;
     if (likedByU == false) {
--- scripts/template.js
+++ scripts/template.js
@@ -47,4 +47,5 @@ document.getElementById('main-container').innerHTML += `
 
             </section>
            `;
+           firstLickedIcon(i);
 }
