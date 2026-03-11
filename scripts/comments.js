99bd792d90d9ed71bc586db2d6067215c3add1d1
Bjoern <onoofelogmail.com>
2026-03-11 17:07:47 +0100

    added function save to local storage (wip)

--- scripts/comments.js
+++ scripts/comments.js
@@ -1,9 +1,12 @@
+
+// pull comments from object, then add them to 1st render.
 function loadComments(index){
+    let locationID = "book-comments-" + index;
+    let commentLocation = document.getElementById(locationID);
+    commentLocation.innerHTML = "";
     for (let i = 0; i < books[index].comments.length; i++) {
         let newCommentName = books[index].comments[i].name;
         let newComment = books[index].comments[i].comment;
-        let locationID = "book-comments-" + index;
-        let commentLocation = document.getElementById(locationID);
         commentLocation.innerHTML += `
         <div>
             <h3>${newCommentName}</h3>
@@ -14,14 +17,11 @@ function loadComments(index){
     }
 }
 
-function addComment(indexComment){
-    let newCommentID = "comment-input-" + indexComment;
-    let newComment = document.getElementById(newCommentID).value;
-    let savedComment = {
-    "name" : "You",
-    };
-    savedComment.push("comment" + ":" + newComment);
 
-    books[indexComment].comments.comment.push(savedComment);
-    console.log(books[indexComment].comments);
+function addComment(indexComment){
+    let inputID = "comment-input-" + indexComment;
+    let newComment = document.getElementById(inputID).value;
+    let savedComment = {"name": "You", "comment": newComment}
+    books[indexComment].comments.push(savedComment);
+    loadComments(indexComment);
 }
--- scripts/price.js
+++ scripts/price.js
@@ -1,10 +1,10 @@
 let currentPrice;
 
-// pull book-price-number from object, extend number by .0001, then depict number up to 2 decimals.
+// pull book-price-number from object, then depict number up to 2 decimals.
 function  renderPrice(index){
-    currentPrice = books[index].price;
-    let extendedNumber = currentPrice + 0.0001; 
-    let fixedPrice = extendedNumber.toFixed(2);
+    currentPrice = books[index].price; 
+    let fixedPrice = currentPrice.toFixed(2);
+    let fixedfixedPrice = fixedPrice.replace(".", ",");
     let bookPriceID = "book-price-" + index;
-    document.getElementById(bookPriceID).innerHTML = fixedPrice +"€";
+    document.getElementById(bookPriceID).innerHTML = fixedfixedPrice +"€";
 }
