c5ece77cd8ace634d10c1a21152f448b31d2d5cb
Bjoern <onoofelogmail.com>
2026-03-11 13:54:26 +0100

    re-placed liked-icon

--- scripts/price.js
+++ scripts/price.js
@@ -1,5 +1,6 @@
 let currentPrice;
 
+// pull book-price-number from object, extend number by .0001, then depict number up to 2 decimals.
 function  renderPrice(index){
     currentPrice = books[index].price;
     let extendedNumber = currentPrice + 0.0001; 
